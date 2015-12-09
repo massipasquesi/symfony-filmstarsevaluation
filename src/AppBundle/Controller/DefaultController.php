b0694f8f2df67863f22119f27c4dff6a9cbfb667
massi <massizero@gmail.com>
2015-12-09 20:56:58 +0100

    one more step .. begin movie & user templates & forms

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Movie;
 
 class DefaultController extends Controller
 {
@@ -18,4 +19,23 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
+
+    /**
+     * @Route("/movie/{id}", name="movie_show")
+     */
+    public function movieShowAction(Movie $movie)
+    {
+        return $this->render('movie/show.html.twig', array(
+            'movie'        => $movie,
+        ));
+    }
+
+    /**
+     * @Route("/movie/list", name="movie_list")
+     */
+    public function movieListAction()
+    {
+        return $this->render('movie/list.html.twig');
+    }
+
 }
---
+++ src/AppBundle/Form/MovieType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('directeur')
+            ->add('year')
+            ->add('evaluations')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Movie'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'movie';
+    }
+}
