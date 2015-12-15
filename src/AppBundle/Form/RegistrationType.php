549d26e7cad44f557c5c509251f9189ac0815500
massi <massizero@gmail.com>
2015-12-16 00:42:43 +0100

    MAJ Forms

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -2,16 +2,24 @@
 
 namespace AppBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
-class RegistrationType extends AbstractType
+class RegistrationType extends BaseType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('avatarFile', 'file');
         $builder->add('first_name');
         $builder->add('last_name');
+        $builder->add('categories_choices', new UserChoiceCategoryType() );
+        $builder->add('age_range', 'entity', array(
+            'class'     => 'AppBundle:AgeRange',
+            'choice_label' => 'age',
+            'expanded'  => false,
+            'multiple'  => false
+        ));
     }
 
     public function getParent()
